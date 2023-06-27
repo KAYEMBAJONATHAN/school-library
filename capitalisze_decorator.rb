97c2092fae77c585d3969112b64a3bf2c316cf87
kayemba <kayembejonathan5@gmail.com>
2023-06-27 18:13:45 +0200

    fix the issue raised by the reviewer

--- capitalisze_decorator.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'decorator'
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    super.Capitalize
-  end
-end
--- decorator.rb
+++ decorator.rb
@@ -1,4 +1,5 @@
 require_relative 'nameable'
+require_relative 'person'
 
 class Decorator < Nameable
   attr_accessor :nameable
@@ -12,3 +13,27 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    super()
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    super()
+    @nameable.correct_name.slice(0, 10)
+  end
+end
+
+person = Person.new(17, 'maximilianus', parent_permission: true)
+
+capitalize_decorator = CapitalizeDecorator.new(person)
+
+puts capitalize_decorator.correct_name
+
+trimmer_decorator = TrimmerDecorator.new(person)
+
+puts trimmer_decorator.correct_name
