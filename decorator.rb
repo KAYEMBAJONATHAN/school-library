1ec4ec661f03c91658d524f448f6a67251234ee6
kayemba <kayembejonathan5@gmail.com>
2023-06-27 14:31:28 +0200

    fix linters

--- decorator.rb
+++ decorator.rb
@@ -1,16 +1,14 @@
 require_relative 'nameable'
 
-class Decorator < Nameable 
-   attr_accessor : nameable
+class Decorator < Nameable
+  attr_accessor :nameable
 
   def initialize(nameable)
     super()
-
     @nameable = nameable
-  end 
+  end
 
   def correct_name
     @nameable.correct_name
-    end
   end
-  
+end
