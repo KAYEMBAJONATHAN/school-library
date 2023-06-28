8e611b0ee9a8945e4d33f95f0df5f92bd8837db6
kayemba <kayembejonathan5@gmail.com>
2023-06-28 14:48:38 +0200

    Add fix the issues raised by the reviewer

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 require 'securerandom'
 
 class Person < Nameable
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -11,6 +11,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
@@ -19,6 +20,10 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   private
